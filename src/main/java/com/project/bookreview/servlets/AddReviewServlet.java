c4e1f5cce17cac21603eadc650e8aab2ebfcf157
Ashutosh Tripathi <ashutoshtripathi6937@gmail.com>
2021-06-05 23:21:09 +0530

    Add files via upload

---
+++ src/main/java/com/project/bookreview/servlets/AddReviewServlet.java
@@ -0,0 +1,158 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.project.bookreview.servlets;
+
+import com.project.bookreview.dao.PublisherDao;
+import com.project.bookreview.entities.Publisher;
+import com.project.bookreview.entities.Review;
+import com.project.bookreview.helper.FactoryProvider;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author ashut
+ */
+@MultipartConfig(fileSizeThreshold = 1024 * 1024 * 10, maxFileSize = 1024 * 1024 * 50, maxRequestSize = 1024 * 1024 * 100)
+public class AddReviewServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+
+            // printing values for testing  
+ /*        System.out.println("******started*********");
+            Enumeration<String> names = request.getParameterNames();
+
+            while (names.hasMoreElements()) {
+                String nn = names.nextElement();
+                System.out.println(nn);
+                System.out.println("*******values********");
+                System.out.println(request.getParameter(nn));
+            }
+            System.out.println("pic-----" + request.getPart("bookpic").getSubmittedFileName());
+            System.out.println("*****ended************");        
+*/
+
+
+
+
+           String authorName=request.getParameter("Aname");
+           String bookName=request.getParameter("Bname");
+           String bookIntro=request.getParameter("Bintro");
+           String bookFav=request.getParameter("Bfav");
+           String bookAud=request.getParameter("Baud");
+            Part part = request.getPart("bookpic");
+
+            
+            
+            
+           
+            try {
+                  // saving the book image in the folder
+                if (part != null) {
+                    String path = request.getSession().getServletContext().getRealPath("/") + "img" + File.separator + part.getSubmittedFileName();
+               //     System.out.println("paaathhhh--" + path);
+                    FileOutputStream fos = new FileOutputStream(path);
+                    InputStream is = part.getInputStream();
+
+                    byte[] data = new byte[is.available()];
+                    is.read(data);
+
+                    fos.write(data);
+                    fos.flush();
+                    fos.close();
+
+                    
+                    
+                    // now saving the review in the db
+                    // changes required- get publisher by session  and set book id properly
+                    PublisherDao pDao=new PublisherDao(FactoryProvider.getFactory());
+                    Publisher publisher =pDao.getPublisherByEmail("a@a");
+                    Review rev=new  Review(1, authorName, bookName, part.getSubmittedFileName(), bookIntro, bookFav, bookAud, publisher)   ;
+                    
+                    int Rid=pDao.addReview(rev);
+                    
+                    
+                    if(Rid>0){
+                        out.println("sucess");
+                    }
+                    else{
+                        // review not added error
+                        out.println("error");
+                    }
+                } else {
+//                     errror 
+                    //response.sendRedirect("/MYSITE/error_page.jsp");
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
